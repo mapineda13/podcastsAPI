4a3f8861ed9e194ef48b07265c14abe20cd9edb3
Milton Pineda <mapineda.13@gmail.com>
2020-09-19 15:31:48 -0600

    added methods to generate json file with top 20 podcasts and replace top 20 with top 20 podcast from the bottom

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request, jsonify, json
+from flask import Flask, jsonify, json
 import urllib.request
 
 app = Flask(__name__)
+
+
+def get_podcasts_info():
+    source = urllib.request.urlopen(
+        'https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/100/explicit.json'
+    ).read()
+    list_podcasts = json.loads(source)
+    list_podcasts = list_podcasts['feed']
+    list_podcasts = list_podcasts['results']
+    return list_podcasts
 
 
 @app.route('/api/v1/show_podcasts/', methods=['GET'])
@@ -17,11 +27,33 @@ def search_podcasts_by_name(artist_name):
     return jsonify(data_podcasts)
 
 
-def get_podcasts_info():
-    source = urllib.request.urlopen(
-        'https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/100/explicit.json'
-    ).read()
-    list_podcasts = json.loads(source)
-    list_podcasts = list_podcasts['feed']
-    list_podcasts = list_podcasts['results']
-    return list_podcasts
+@app.route('/api/v1/save_top20_podcasts/', methods=['POST'])
+def save_top20_podcasts():
+    data_podcasts = get_podcasts_info()
+    iterator = 0
+    podcasts_list = []
+    for podcast in data_podcasts:
+        podcasts_list.append(podcast)
+        iterator += 1
+        if iterator == 20:
+            break
+    with open('json/top_20_podcasts.json', 'w') as json_file:
+        json.dump(podcasts_list, json_file)
+    return jsonify(podcasts_list)
+
+
+@app.route('/api/v1/save_top20_from_bottom_podcasts/', methods=['POST'])
+def save_top20_from_bottom_podcasts():
+    data_podcasts = get_podcasts_info()
+    iterator = 0
+    reverse_data_podcasts = data_podcasts[::-1]
+    podcasts_list = []
+    for podcast in reverse_data_podcasts:
+        podcasts_list.append(podcast)
+        iterator += 1
+        if iterator == 20:
+            break
+    with open('json/top_20_podcasts.json', 'w') as json_file:
+        json.dump(podcasts_list, json_file)
+    return jsonify(podcasts_list)
+

6f19cff380fba527cdd06ef22d034d179cba8fd4
Milton Pineda <mapineda.13@gmail.com>
2020-09-19 14:14:52 -0600

    adding methods to get podcasts from Json file and method to get info about podcast by name

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ import urllib.request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def show_info():
-    return get_podcasts_info()
+@app.route('/api/v1/show_podcasts/', methods=['GET'])
+def show_podcasts_info():
+    data_podcasts = get_podcasts_info()
+    return jsonify(data_podcasts)
+
+
+@app.route('/api/v1/show_podcasts_by_name/<artist_name>', methods=['GET'])
+def search_podcasts_by_name(artist_name):
+    data_podcasts = get_podcasts_info()
+    data_podcasts = list(filter(lambda x: x["artistName"] == artist_name.strip(), data_podcasts))
+    return jsonify(data_podcasts)
 
 
 def get_podcasts_info():
@@ -14,6 +22,6 @@ def get_podcasts_info():
         'https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/100/explicit.json'
     ).read()
     list_podcasts = json.loads(source)
+    list_podcasts = list_podcasts['feed']
+    list_podcasts = list_podcasts['results']
     return list_podcasts
-
-
